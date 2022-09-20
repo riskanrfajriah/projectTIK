d991876577186d952da04e28f0e73fa9a13d855f
riskanrfajriah <2225210079@untirta.ac.id>
2022-09-20 17:22:38 +0700

    projectTIK

---
+++ faperta/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def faperta(request):
+    return render(request, 'indexfaperta.html')
+   
---
+++ pascasarjana/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pascasarjana(request):
+    return render(request, 'indexpascasarjana.html')
